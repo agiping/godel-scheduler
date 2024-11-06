569b941689814f9c119a167903edc8b8fecf5411
zhangping <zhangping@baichuan-inc.com>
2024-11-06 16:49:49 +0800

    debugging and fixing compare function

--- pkg/scheduler/framework/preemption-plugins/sorting/flextopo/optimal_flextopo.go
+++ pkg/scheduler/framework/preemption-plugins/sorting/flextopo/optimal_flextopo.go
@@ -21,6 +21,7 @@ import (
 
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/util/sets"
+	"k8s.io/klog/v2"
 
 	framework "github.com/kubewharf/godel-scheduler/pkg/framework/api"
 	"github.com/kubewharf/godel-scheduler/pkg/scheduler/framework/handle"
@@ -61,17 +62,39 @@ func (o *OptimalFlextopo) Name() string {
 }
 
 func (o *OptimalFlextopo) Compare(c1, c2 *framework.Candidate) int {
+	// testing purpose
+	klog.Infof("======= OptimalFlextopo Plugin: Comparing candidate==========")
+	victimNames1 := []string{}
+	for _, pod := range c1.Victims.Pods {
+		victimNames1 = append(victimNames1, pod.Name)
+	}
+	klog.Infof("======= Victims of candidate c1: %s: %v", c1.Name, victimNames1)
+
+	victimNames2 := []string{}
+	for _, pod := range c2.Victims.Pods {
+		victimNames2 = append(victimNames2, pod.Name)
+	}
+	klog.Infof("======= Victims of candidate c2: %s: %v", c2.Name, victimNames2)
+	// testing done
+
 	prioritySum1 := getPrioritySum(c1)
 	prioritySum2 := getPrioritySum(c2)
 	alignmentScore1 := getAlignmentScore(c1)
 	alignmentScore2 := getAlignmentScore(c2)
 	score1 := getScore(prioritySum1, alignmentScore1)
 	score2 := getScore(prioritySum2, alignmentScore2)
-	if score1 < score2 {
+	klog.Infof("======= OptimalFlextopo Plugin: Score of c1: %f, score of c2: %f", score1, score2)
+
+	// according to the selection policy of Godel, Candidate[0] is the best;
+	// thus, we should put the higher score at the left side.
+	if score1 > score2 {
+		klog.Infof("======= OptimalFlextopo Plugin: c1 is better than c2")
 		return 1
-	} else if score1 > score2 {
+	} else if score1 < score2 {
+		klog.Infof("======= OptimalFlextopo Plugin: c1 is worse than c2")
 		return -1
 	} else {
+		klog.Infof("======= OptimalFlextopo Plugin: c1 and c2 are the same")
 		return 0
 	}
 }
